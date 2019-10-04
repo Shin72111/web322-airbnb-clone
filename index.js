1bd7c90a882802f8da8b020495e602c96d9f8ccf
Shin72111 <tam.tran72111@gmail.com>
2019-10-04 16:33:46 -0400

    Finish assignment 1

--- index.js
+++ index.js
@@ -17,12 +17,83 @@ app.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-app.post("/signup", (req, res) => {
-  console.log(typeof req.body.birthday);
-  const date = new Date(req.body.birthday);
-  console.log(date.toISOString());
-  console.log(req.body);
-  res.render("signup");
+app.get("/rooms", (req, res) => {
+  const rooms = [
+    {
+      image: {
+        src:
+          "https://a0.muscache.com/im/pictures/10833886/1edf8559_original.jpg?aki_policy=large",
+        alt: "Entire apartment - HVAR image"
+      },
+      info: "Entire apartment - HVAR",
+      title: "The best position in Hvar!",
+      price: 88,
+      rating: "4.77",
+      bookCount: 366
+    },
+    {
+      image: {
+        src:
+          "https://a0.muscache.com/im/pictures/19327619/cbb26fd1_original.jpg?aki_policy=large",
+        alt: "Entire apartment - Minsk image"
+      },
+      info: "Entire apartment - Minsk",
+      title: "Minsk Belarus Studio in historical center",
+      price: 60,
+      rating: "4.79",
+      bookCount: 344
+    },
+    {
+      image: {
+        src:
+          "https://a0.muscache.com/im/pictures/10026520/717f5adc_original.jpg?aki_policy=large",
+        alt: "Private room - Ermelo image"
+      },
+      info: "Private room - Ermelo",
+      title: "Charming gardenroom with woodstove",
+      price: 78,
+      rating: "4.80",
+      bookCount: 62
+    },
+    {
+      image: {
+        src:
+          "https://a0.muscache.com/4ea/air/v2/pictures/746ce656-ed8d-4966-8fcb-e8c1ce4f39d7.jpg?t=r:w1200-h720-sfit,e:fjpg-c90",
+        alt: "Verified - Menaggio image"
+      },
+      info: "Verified - Menaggio",
+      title: "Romantic, Lakeside Home with Views of Lake Como",
+      price: 211,
+      rating: "4.85",
+      bookCount: 233
+    },
+    {
+      image: {
+        src:
+          "https://a0.muscache.com/im/pictures/cd17b75f-9aee-4f68-b80d-dde84996fb4b.jpg?aki_policy=large",
+        alt: "Entire house - Isla Mujeres image"
+      },
+      info: "Entire house - Isla Mujeres",
+      title: "The World Famous Seashell House ~ Casa Caracol",
+      price: 395,
+      rating: "4.76",
+      bookCount: 247
+    },
+    {
+      image: {
+        src:
+          "https://a0.muscache.com/im/pictures/15273358/d7329e9a_original.jpg?aki_policy=large",
+        alt: "Entire house - Ostuni image"
+      },
+      info: "Entire house - Ostuni",
+      title: "I SETTE CONI - TRULLO EDERA ",
+      price: 102,
+      rating: "4.93",
+      bookCount: 193
+    }
+  ];
+
+  res.render("rooms", { rooms });
 });
 
 const PORT = process.env.PORT || 3000;
--- public/js/script.js
+++ public/js/script.js
@@ -39,3 +39,82 @@ document.addEventListener("DOMContentLoaded", () => {
   // }
   window.calendars = calendars;
 });
+
+const configGuestBtn = () => {
+  const update_display = () => {
+    const adult_no = parseInt(document.querySelector(`#adult-num`).innerHTML);
+    const children_no = parseInt(
+      document.querySelector(`#children-num`).innerHTML
+    );
+
+    let display;
+    if (adult_no > 0) {
+      display = `${adult_no} adults`;
+      if (children_no > 1) {
+        display += `, ${children_no} children`;
+      } else if (children_no == 1) {
+        display += `, 1 child`;
+      }
+    } else {
+      display = "Guests";
+    }
+
+    document.querySelector("#guest-display").innerHTML = display;
+  };
+
+  const change_no = (no_id, no_input_id, change) => {
+    const no_el = document.querySelector(`#${no_id}`);
+    let no = parseInt(no_el.innerHTML) + change;
+    no = no < 0 ? 0 : no;
+    document.querySelector(`#${no_input_id}`).value = no;
+    no_el.innerHTML = no;
+    update_display();
+    return no;
+  };
+
+  const minus_adults_btn = document.querySelector("#minus-adult-btn");
+  const minus_children_btn = document.querySelector("#minus-children-btn");
+
+  minus_adults_btn.addEventListener("click", () => {
+    const no = change_no("adult-num", "adults", -1);
+    if (no == 0) {
+      minus_adults_btn.setAttribute("disabled", "");
+      minus_children_btn.setAttribute("disabled", "");
+      while (change_no("children-num", "children", -1) != 0);
+    }
+  });
+
+  document.querySelector("#plus-adult-btn").addEventListener("click", () => {
+    change_no("adult-num", "adults", 1);
+    minus_adults_btn.removeAttribute("disabled");
+  });
+
+  minus_children_btn.addEventListener("click", () => {
+    const no = change_no("children-num", "children", -1);
+    if (no == 0) {
+      minus_children_btn.setAttribute("disabled", "");
+    }
+  });
+  document.querySelector("#plus-children-btn").addEventListener("click", () => {
+    const adult_no = change_no("adult-num", "adults", 0);
+    if (adult_no == 0) {
+      change_no("adult-num", "adults", 1);
+      minus_adults_btn.removeAttribute("disabled");
+    }
+    change_no("children-num", "children", 1);
+    minus_children_btn.removeAttribute("disabled");
+  });
+};
+
+const configGuestDropDown = () => {
+  configGuestBtn();
+  document
+    .querySelector(".dropdown-trigger.book-dropdown")
+    .addEventListener("click", () => {
+      // Get the target from the "data-target" attribute
+      const target = document.querySelector(".dropdown.book-dropdown");
+      target.classList.toggle("is-active");
+    });
+};
+
+configGuestDropDown();
